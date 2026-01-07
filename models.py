545d9e5bb9169af1eaf085734c83edbcde0179a5
Amir <amirkhanimonfared85@gmail.com>
2026-01-07 16:23:08 +0330

    changed file models python with func

---
+++ models.py
@@ -0,0 +1,14 @@
+import random
+
+
+
+def func():
+
+	return (a * b) / c
+
+
+a = int(input("Enter a number_1:"))
+b = int(input("Enter a number_2:"))
+c = int(input("Enter a number_3:"))
+
+print(func())
